33c791f233d33efe4209dd344bbfa4fc350f5a53
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-20 17:42:10 +0200

    feat: implement the fake data stories

---
+++ data/fakeStories.ts
@@ -0,0 +1,35 @@
+interface Users {
+  source: String | any;
+  user: String | any;
+  name: String;
+  cheked: boolean;
+}
+
+const fakeStorys: Users[] = [
+  {
+    source: require("../assets/story5.jpg"),
+    user: require("../assets/28.jpg"),
+    name: "Frankie Stein",
+    cheked: false,
+  },
+  {
+    source: require("../assets/story4.jpg"),
+    user: require("../assets/40.jpg"),
+    name: "Morty Jones",
+    cheked: false,
+  },
+  {
+    source: require("../assets/story3.jpg"),
+    user: require("../assets/68.jpg"),
+    name: "King Kobra",
+    cheked: true,
+  },
+  {
+    source: require("../assets/story2.jpg"),
+    user: require("../assets/8.jpg"),
+    name: "Turu Leta",
+    cheked: false,
+  },
+];
+
+export default fakeStorys;
